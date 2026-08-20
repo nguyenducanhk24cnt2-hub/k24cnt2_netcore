da1a1971576987a4050610b02160f5a7d00bfdf3
nguyenducanhk24cnt2-hub <nguyenducanh12a7nhnd.com>
2026-08-20 21:15:28 +0700

    NdaLesson01

---
+++ NdaLesson01Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NdaLesson01Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
