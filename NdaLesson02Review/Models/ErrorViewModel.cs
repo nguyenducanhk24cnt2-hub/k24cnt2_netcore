649dce5ef8708e92dd547a9d28744e95c859b517
nguyenducanhk24cnt2-hub <nguyenducanh12a7nhnd.com>
2026-08-26 13:42:31 +0700

    Ndalesson02

---
+++ NdaLesson02Review/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NdaLesson02Review.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
